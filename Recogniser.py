1ed64138f6bff0e8cd2ca6675e2f5b81d8ef8e64
Roberta Macaluso <roby_97@outlook.it>
2022-11-09 19:58:21 +0100

    I tried to calculate distances with signs

--- Recogniser.py
+++ Recogniser.py
@@ -15,7 +15,23 @@ tol = 15
 # si confonde tra: r, u e v / t, x / c_circonflesso, o, p (quest'ultimi più raramente)
 
 
-def findDistances(lmList): # calculates, for each node, its distance with all the 21 nodes (with itself too and it's 0)
+def find_distances(lmList):
+    # calculates, for each node, its signed distance along x and y with all the 21 nodes (with itself too and it's 0)
+    distMatrix = []
+    dist_matrix_x = np.zeros([len(lmList), len(lmList)], dtype='float')
+    dist_matrix_y = np.zeros([len(lmList), len(lmList)], dtype='float')
+    palmSize = ((lmList[0][1] - lmList[9][1]) ** 2 + (lmList[0][2] - lmList[9][2]) ** 2) ** (1. / 2.)
+
+    for row in range(0, len(lmList)):
+        for column in range(0, len(lmList)):
+            dist_matrix_x[row][column] = (lmList[row][1] - lmList[column][1])/palmSize
+            dist_matrix_y[row][column] = (lmList[row][2] - lmList[column][2])/palmSize
+
+    distMatrix = [dist_matrix_x, dist_matrix_y]
+    return distMatrix
+
+
+def find_distances_module(lmList): # calculates, for each node, its distance with all the 21 nodes (with itself too and it's 0)
     distMatrix = np.zeros([len(lmList), len(lmList)], dtype='float')
     palmSize = ((lmList[0][1]-lmList[9][1])**2+(lmList[0][2]-lmList[9][2])**2)**(1./2.)
     for row in range(0, len(lmList)):
@@ -24,7 +40,7 @@ def findDistances(lmList): # calculates, for each node, its distance with all th
     return distMatrix
 
 
-def findGestureNew(unknownGesture, knownGestures, keyPoints, gestNames, tol):
+def find_gesture(unknownGesture, knownGestures, keyPoints, gestNames, tol):
     # unknown gesture: gesture detected from webcam
     # knownGestures: array of all the gesture for each letter
     # keyPoints: all the key id of the hand
@@ -36,36 +52,7 @@ def findGestureNew(unknownGesture, knownGestures, keyPoints, gestNames, tol):
     # For each gestName he finds the error between the gesture related to that Name and
     # the real time detected gesture
     for i in range(0, len(gestNames), 1):
-        error = findError(knownGestures[i], unknownGesture, keyPoints)
-        errorArray.append(error)
-    errorMin = errorArray[0]
-    minIndex = 0
-
-    # finds a group of candidates
-    candidates = []
-    for i in range(0, len(errorArray), 1):
-        if errorArray[i] < tol:
-            candidates.append(errorArray[i])
-
-    if len(candidates) == 0:
-        gesture = 'Unknown'
-
-    return gesture
-
-
-def findGesture(unknownGesture, knownGestures, keyPoints, gestNames, tol):
-    # unknown gesture: gesture detected from webcam
-    # knownGestures: array of all the gesture for each letter
-    # keyPoints: all the key id of the hand
-    # gestNames: array of all the Letters Names
-    # tol: constant for error
-    gesture = 'Unknown'
-
-    errorArray = []
-    # For each gestName he finds the error between the gesture related to that Name and
-    # the real time detected gesture
-    for i in range(0, len(gestNames), 1):
-        error = findError(knownGestures[i], unknownGesture, keyPoints)
+        error = find_error(knownGestures[i], unknownGesture, keyPoints)
         errorArray.append(error)
     errorMin = errorArray[0]
     minIndex = 0
@@ -82,7 +69,49 @@ def findGesture(unknownGesture, knownGestures, keyPoints, gestNames, tol):
     return gesture
 
 
-def findError(gestureMatrix, unknownMatrix, keyPoints):
+def find_gesture_module(unknownGesture, knownGestures, keyPoints, gestNames, tol):
+    # unknown gesture: gesture detected from webcam
+    # knownGestures: array of all the gesture for each letter
+    # keyPoints: all the key id of the hand
+    # gestNames: array of all the Letters Names
+    # tol: constant for error
+    gesture = 'Unknown'
+
+    errorArray = []
+    # For each gestName he finds the error between the gesture related to that Name and
+    # the real time detected gesture
+    for i in range(0, len(gestNames), 1):
+        error = find_error_module(knownGestures[i], unknownGesture, keyPoints)
+        errorArray.append(error)
+    errorMin = errorArray[0]
+    minIndex = 0
+
+    # finds the min in error array
+    for i in range(0, len(errorArray), 1):
+        if errorArray[i] < errorMin:
+            errorMin = errorArray[i]
+            minIndex = i
+    if errorMin < tol:
+        gesture = gestNames[minIndex]
+    if errorMin >= tol:
+        gesture = 'Unknown'
+    return gesture
+
+
+def find_error(gestureMatrix, unknownMatrix, keyPoints):
+    errorX = 0
+    errorY = 0
+
+    for row in range(0, 20):
+        for column in range(0, 20):
+            errorX = errorX + abs(gestureMatrix[0][row][column] - unknownMatrix[0][row][column])
+            errorY = errorY + abs(gestureMatrix[1][row][column] - unknownMatrix[1][row][column])
+
+    error = [errorX, errorY]
+    return error
+
+
+def find_error_module(gestureMatrix, unknownMatrix, keyPoints):
     error = 0
     for row in keyPoints:
         for column in keyPoints:
@@ -90,19 +119,19 @@ def findError(gestureMatrix, unknownMatrix, keyPoints):
     return error
 
 
-def fpsShow(img, pTime):
+def fps_show(img, pTime):
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     cv2.putText(img, f'FPS: {int(fps)}', (500, 450), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
     return img, cTime
 
 
-def getLetter(fileName):
+def get_letter(fileName):
     x = fileName.split('.')
     return x[0]
 
 
-def getKnownGestures(path):
+def get_known_gestures(path):
     gesturesList = []
     namesList = []
     for filename in os.listdir(path):
@@ -111,7 +140,21 @@ def getKnownGestures(path):
             gesture = json.loads(gesture)
             gesturesList.append(gesture)
 
-            namesList.append(getLetter(filename))
+            namesList.append(get_letter(filename))
+            outfile.close()
+    return namesList, gesturesList  # np.array(gesturesList)
+
+
+def get_known_gestures_module(path):
+    gesturesList = []
+    namesList = []
+    for filename in os.listdir(path):
+        with open(str(path+filename), 'r') as outfile:
+            gesture = outfile.read()
+            gesture = json.loads(gesture)
+            gesturesList.append(gesture)
+
+            namesList.append(get_letter(filename))
             outfile.close()
     return namesList, gesturesList  # np.array(gesturesList)
 
@@ -134,6 +177,35 @@ def handle_close(event, cap):
     cap.release()
 
 
+def grab_frame_module(cap, detector, gestNames, knownGestures, pTime):
+    """
+    Method to grab a frame from the camera and elaborate it in order to detect the hand and signs
+    :param cap: the VideoCapture object
+    :return: the captured image
+    """
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    img = detector.findHands(img)
+    RightHand = detector.RightHand(img)  # False = Left Hand; True = Right Hand
+    lmList = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0 and RightHand is False:  # if a hand is detected
+        unknownGesture = find_distances_module(lmList)
+        myGesture = find_gesture_module(unknownGesture, knownGestures, keyPoints, gestNames, tol)
+        fingers_up, fingers_names = fUDd.find_fingers_up(lmList)
+        text = myGesture
+        # cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        # cv2.putText(img, text, (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
+        cv2.putText(img, text, (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 125), 3, cv2.LINE_AA)
+        cv2.putText(img, fingers_names, (0, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (30, 144, 255), 2, cv2.LINE_AA)
+
+    if RightHand is True:
+        cv2.putText(img, "Remove your Right Hand", (2, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 125), 2, cv2.LINE_AA)
+
+    # img, pTime = fps_show(img, pTime)  # Show fps number
+
+    return img, pTime
+
 def grab_frame(cap, detector, gestNames, knownGestures, pTime):
     """
     Method to grab a frame from the camera and elaborate it in order to detect the hand and signs
@@ -147,8 +219,8 @@ def grab_frame(cap, detector, gestNames, knownGestures, pTime):
     lmList = detector.findPosition(img, draw=False)
 
     if len(lmList) != 0 and RightHand is False:  # if a hand is detected
-        unknownGesture = findDistances(lmList)
-        myGesture = findGesture(unknownGesture, knownGestures, keyPoints, gestNames, tol)
+        unknownGesture = find_distances(lmList)
+        myGesture = find_gesture(unknownGesture, knownGestures, keyPoints, gestNames, tol)
         fingers_up, fingers_names = fUDd.find_fingers_up(lmList)
         text = myGesture
         # cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
@@ -159,9 +231,10 @@ def grab_frame(cap, detector, gestNames, knownGestures, pTime):
     if RightHand is True:
         cv2.putText(img, "Remove your Right Hand", (2, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 125), 2, cv2.LINE_AA)
 
-    # img, pTime = fpsShow(img, pTime)  # Show fps number
+    # img, pTime = fps_show(img, pTime)  # Show fps number
 
     return img, pTime
+
 
 def main():
     pTime = 0
@@ -182,11 +255,12 @@ def main():
 
     # prep detector + load known gestures
     detector = htm.handDetector(detectionCon=1)
-    gestNames, knownGestures = getKnownGestures("GesturesFiles/")
+    gestNames, knownGestures = get_known_gestures("GesturesFilesModules/")
 
     while cap.isOpened():
         # get the current frame
-        frame, pTime = grab_frame(cap, detector, gestNames, knownGestures, pTime)
+        frame, pTime = grab_frame_module(cap, detector, gestNames, knownGestures, pTime)
+        # frame, pTime = grab_frame(cap, detector, gestNames, knownGestures, pTime)
 
         if img is None:
             # convert it in RBG (for Matplotlib)
--- StoreGestures.py
+++ StoreGestures.py
@@ -10,47 +10,96 @@ import matplotlib.pyplot as plt
 samples = 100
 start = False
 path = "GesturesFiles/"
+path_module = "GesturesFilesModules/"
+
+def find_distances(lmList):
+    # calculates, for each node, its signed distance along x and y with all the 21 nodes (with itself too and it's 0)
+    dist_matrix_x = np.zeros([len(lmList), len(lmList)], dtype='float')
+    dist_matrix_y = np.zeros([len(lmList), len(lmList)], dtype='float')
+    palmSize = ((lmList[0][1] - lmList[9][1]) ** 2 + (lmList[0][2] - lmList[9][2]) ** 2) ** (1. / 2.)
+
+    for row in range(0, len(lmList)):
+        for column in range(0, len(lmList)):
+            dist_matrix_x[row][column] = (lmList[row][1] - lmList[column][1])/palmSize
+            dist_matrix_y[row][column] = (lmList[row][2] - lmList[column][2])/palmSize
+
+    distMatrix = [dist_matrix_x, dist_matrix_y]
+    return distMatrix
 
 
-def findDistances(lmList):  # calculates, for each node, its distance with all the 21 nodes (with itself too and it's 0)
+def find_distances_module(lmList):
+    # calculates, for each node, its distance with all the 21 nodes (with itself too and it's 0)
     distMatrix = np.zeros([len(lmList), len(lmList)], dtype='float')
     palmSize = ((lmList[0][1] - lmList[9][1]) ** 2 + (lmList[0][2] - lmList[9][2]) ** 2) ** (1. / 2.)
     for row in range(0, len(lmList)):
         for column in range(0, len(lmList)):
-            distMatrix[row][column] = (((lmList[row][1] - lmList[column][1]) ** 2 + (
-                        lmList[row][2] - lmList[column][2]) ** 2) ** (1. / 2.)) / palmSize
+            distMatrix[row][column] = (((lmList[row][1] - lmList[column][1]) ** 2 +
+                                        (lmList[row][2] - lmList[column][2]) ** 2) ** (1. / 2.)) / palmSize
     return distMatrix
 
 
-def CalculateAverage(samplesList):
+def calculate_average(samplesList):
     if len(samplesList) > 0:
-        sumG = samplesList[0]
+        sumX = samplesList[0][0]
+        sumY = samplesList[0][1]
         for i in range(1, len(samplesList)):
-            sumG = np.add(sumG, samplesList[i])
-        avg = sumG/len(samplesList)
-        print(avg)
+            sumX = np.add(sumX, samplesList[i][0])
+            sumY = np.add(sumY, samplesList[i][1])
+        avgX = sumX/len(samplesList)
+        avgY = sumX / len(samplesList)
+
+        avg = [avgX, avgY]
         return avg
     else:
         return -1
 
 
-def saveInFile(letterGestureAVG, letterName):
+def calculate_average_module(samplesList):
+    if len(samplesList) > 0:
+        sumG = samplesList[0]
+        for i in range(1, len(samplesList)):
+            sumG = np.add(sumG, samplesList[i])
+        avg = sumG/len(samplesList)
+        return avg
+    else:
+        return -1
+
+
+def save_in_file(letterGestureAVG, letterName):
+    print('Saving in json')
+
+    # Serializing json
+    json_object_x = json.dumps(letterGestureAVG[0].tolist())
+    json_object_y = json.dumps(letterGestureAVG[1].tolist())
+
+    # Writing to sample.json
+    with open(path + letterName.upper() + ".json", "w+") as outfile:
+        outfile.write('[')
+        outfile.write(json_object_x)
+        outfile.write(', ')
+        outfile.write(json_object_y)
+        outfile.write(']')
+        outfile.close()
+
+
+def save_in_file_module(letterGestureAVG, letterName):
     print('Saving in json')
 
     # Serializing json
     json_object = json.dumps(letterGestureAVG.tolist())
 
     # Writing to sample.json
-    with open(path + letterName.upper() + ".json", "w+") as outfile:
+    with open(path_module + letterName.upper() + ".json", "w+") as outfile:
         outfile.write(json_object)
         outfile.close()
 
 
-def fpsShow(img, pTime):
+def fps_show(img, pTime):
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     cv2.putText(img, f'FPS: {int(fps)}', (500, 450), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
     return cTime
+
 
 def bgr_to_rgb(image):
     """
@@ -70,17 +119,17 @@ def handle_close(event, cap):
     cap.release()
 
 
-def startC():
+def start_c():
     prompt = 'Press S when Ready --> '
     name = input(prompt)
     if name == 's' or name == 'S':
         global start
         start = True
     else:
-        startC()
+        start_c()
 
 
-def StoreGestures(_letter):
+def store_gestures(_letter):
     pTime = 0
 
     # init the camera
@@ -99,14 +148,14 @@ def StoreGestures(_letter):
 
     # prep detector + init unknownGestureSamples list
     detector = htm.handDetector(detectionCon=1)
-    unknownGestureSamples = []
+    unknown_gesture_samples = []
 
     # flags
     i = 0
     averageFlag = True
 
     # Throw a thread that controls the start of the calculating
-    t = Thread(target=startC)
+    t = Thread(target=start_c)
     t.start()
 
     while cap.isOpened():
@@ -123,13 +172,13 @@ def StoreGestures(_letter):
                 if i < samples:
                     cv2.putText(frame, 'Storing ' + _letter.upper(), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 125),
                                 3, cv2.LINE_AA)
-                    unknownGestureSample = findDistances(lmList)  # save the sample
-                    unknownGestureSamples.append(unknownGestureSample)  # add the sample to the list of samples
+                    unknown_gesture_sample = find_distances(lmList)  # save the sample
+                    unknown_gesture_samples.append(unknown_gesture_sample)  # add the sample to the list of samples
                     i = i + 1
                 else:
                     if averageFlag is True:
-                        res = CalculateAverage(unknownGestureSamples)  # calculate the average
-                        saveInFile(res, letter)  # save the result into a file
+                        res = calculate_average(unknown_gesture_samples)  # calculate the average
+                        save_in_file(res, letter)  # save the result into a file
                         averageFlag = False
                     else:
                         return
@@ -138,7 +187,7 @@ def StoreGestures(_letter):
             cv2.putText(frame, "Remove your Right Hand", (2, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 125), 2,
                         cv2.LINE_AA)
 
-        # frame, pTime = fpsShow(frame, pTime)  # Show fps number
+        # frame, pTime = fps_show(frame, pTime)  # Show fps number
 
         if img is None:
             # convert it in RBG (for Matplotlib)
@@ -155,10 +204,86 @@ def StoreGestures(_letter):
             fig.canvas.flush_events()
             plt.pause(1 / 30)
 
+def store_gestures_module(_letter):
+    pTime = 0
+
+    # init the camera
+    cap = cv2.VideoCapture(0)
+
+    # enable Matplotlib interactive mode
+    plt.ion()
+
+    # create a figure to be updated
+    fig = plt.figure()
+    # intercept the window's close event to call the handle_close() function
+    fig.canvas.mpl_connect("close_event", lambda event: handle_close(event, cap))
+
+    # prep a variable for the first run
+    img = None
+
+    # prep detector + init unknownGestureSamples list
+    detector = htm.handDetector(detectionCon=1)
+    unknown_gesture_samples = []
+
+    # flags
+    i = 0
+    averageFlag = True
+
+    # Throw a thread that controls the start of the calculating
+    t = Thread(target=start_c)
+    t.start()
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+
+        frame = detector.findHands(frame)
+        RightHand = detector.RightHand(frame)  # False = Left Hand;   True = Right Hand
+        lmList = detector.findPosition(frame, draw=False)
+        # print(lmList)
+
+        if len(lmList) != 0 and RightHand is False:  # if a only left hand is detected
+            if start is True:
+                if i < samples:
+                    cv2.putText(frame, 'Storing ' + _letter.upper(), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 125),
+                                3, cv2.LINE_AA)
+                    unknown_gesture_sample = find_distances_module(lmList)  # save the sample
+                    unknown_gesture_samples.append(unknown_gesture_sample)  # add the sample to the list of samples
+                    i = i + 1
+                else:
+                    if averageFlag is True:
+                        res = calculate_average_module(unknown_gesture_samples)  # calculate the average
+                        save_in_file_module(res, letter)  # save the result into a file
+                        averageFlag = False
+                    else:
+                        return
+
+        elif RightHand is True:
+            cv2.putText(frame, "Remove your Right Hand", (2, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 125), 2,
+                        cv2.LINE_AA)
+
+        # frame, pTime = fps_show(frame, pTime)  # Show fps number
+
+        if img is None:
+            # convert it in RBG (for Matplotlib)
+            img = plt.imshow(bgr_to_rgb(frame))
+            plt.axis("off")  # hide axis, ticks, ...
+            plt.title("Store Gestures")
+            # show the plot!
+            plt.show()
+        else:
+            # set the current frame as the data to show
+            img.set_data(bgr_to_rgb(frame))
+            # update the figure associated to the shown plot
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+            plt.pause(1 / 30)
+
+
 if __name__ == "__main__":
     try:
         prompt = 'Which Letter? --> '
         letter = input(prompt)
-        StoreGestures(letter)
+        store_gestures_module(letter)
     except KeyboardInterrupt:
         exit(0)
